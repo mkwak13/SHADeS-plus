d53acbece676e10cc204095961c1dcfb60a19c1a
Minseok <minseok.kwak1438@gmail.com>
2026-02-26 06:13:38 +0000

    .

--- trainer.py
+++ trainer.py
@@ -560,7 +560,8 @@ class Trainer:
             photo = self.compute_reprojection_loss(raw, pred)
 
             M_soft = outputs[("mask", 0, 0)].detach()
-            reprojection_loss_item = photo * (1.0 - M_soft)
+            alpha = 0.3
+            reprojection_loss_item = photo * (1.0 - alpha * M_soft)
 
             if self.opt.automasking:
                 identity_reprojection_loss_item = self.compute_reprojection_loss(inputs[("color", frame_id, 0)], inputs[("color", 0, 0)])
