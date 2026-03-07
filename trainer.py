e1df661e6ef9bf167f42d56b449e8354bbb67e00
Minseok <minseok.kwak1438@gmail.com>
2026-03-07 17:46:33 +0000

    .

--- trainer.py
+++ trainer.py
@@ -53,7 +53,7 @@ class Trainer:
         self.models["encoder"] = networks.ResnetEncoder(
             self.opt.num_layers,
             self.opt.weights_init == "pretrained",
-            num_input_images=2   # ? ??
+            num_input_images=3  # ? ??
         )
         self.models["encoder"].to(self.device)
         self.parameters_to_train += list(self.models["encoder"].parameters())
@@ -371,8 +371,9 @@ class Trainer:
         else:
             input_color = inputs[("color_aug", 0, 0)]
             reflectance = outputs[("reflectance", 0, 0)]
+            mask = outputs[("mask", 0, 0)]
 
-            depth_input = torch.cat([input_color, reflectance], dim=1)
+            depth_input = torch.cat([input_color, reflectance, mask], dim=1)
 
             features = self.models["encoder"](depth_input)
         outputs.update(self.models["depth"](features))
